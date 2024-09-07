3f25e7b7cee9f4b3368cae62308463f519ad4dce
cristian-celis <jesuscelis468@gmail.com>
2024-09-07 10:47:53 -0500

    add .gitignore

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const app = express();
 const PORT = 3000;
 
-// Configurar Multer para el manejo de archivos
+// Configurar Multer para manejar archivos
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadDir = './uploads';
@@ -18,42 +18,37 @@ const storage = multer.diskStorage({
   },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
-    cb(null, `${uuidv4()}${ext}`); // Genera un nombre único para el archivo.
+    cb(null, `${uuidv4()}${ext}`); // Generar codigo unico
   }
 });
+
 const upload = multer({ storage });
 
-// Almacenar las citas en memoria
-let citas = [];
+let appointments = []; // Almacenar citas en memoria
 
-// Endpoint para crear una nueva cita médica
-app.post('/citas', upload.single('autorizacion'), (req, res) => {
-  const { cc, fecha } = req.body;
+// Crear cita medica
+app.post('/appointments', upload.single('authorization'), (req, res) => {
+  const { cc, date } = req.body;
   const file = req.file;
 
-  if (!cc || !fecha || !file) {
+  if (!cc || !date || !file) {
     return res.status(400).json({ message: 'Faltan datos o archivo' });
   }
 
-  // Generar código único para la cita
-  const codigoCita = uuidv4();
+  const appointmentCode = uuidv4();
 
-  // Crear objeto de la cita
-  const nuevaCita = {
-    codigo: codigoCita,
+  const newAppointment = {
+    code: appointmentCode,
     cc,
-    fecha,
-    autorizacion: file.path
+    date,
+    authorization: file.path
   };
 
-  // Guardar la cita en memoria
-  citas.push(nuevaCita);
+  appointments.push(newAppointment);
 
-  // Retornar el código de la cita
-  res.json({ codigo: codigoCita });
+  res.json({ code: appointmentCode });
 });
 
-// Iniciar el servidor
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
