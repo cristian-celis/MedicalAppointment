56bd5a0c845f524aa621b4c37672035491e02bd4
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2024-09-08 16:41:13 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
@@ -6,6 +7,8 @@ const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
+
+
 
 // Configurar Multer para manejar archivos
 const storage = multer.diskStorage({
@@ -24,7 +27,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
-let appointments = []; // Almacenar citas en memoria
+const appointments = []; // Almacenar citas en memoria
 
 // Crear cita medica
 app.post('/appointments', upload.single('authorization'), (req, res) => {
@@ -41,6 +44,7 @@ app.post('/appointments', upload.single('authorization'), (req, res) => {
     code: appointmentCode,
     cc,
     date,
+    status: true,
     authorization: file.path
   };
 
@@ -49,6 +53,46 @@ app.post('/appointments', upload.single('authorization'), (req, res) => {
   res.json({ code: appointmentCode });
 });
 
+app.get("/appointments", (_req, res) => {
+  const { date1, date2 } = _req.query;
+  if (!date1 || !date2) {
+    return res.status(400).json({ message: 'Falta rango de fechas' });
+  }
+
+  function filterAppointments(date1,date2){
+    const start = new Date(date1);
+    const end = new Date(date2);
+    return appointments.filter(item => {
+      const itemDate = new Date(item.date);
+      return itemDate >= start && itemDate <= end;
+  });
+  }
+  const filteredAppointments= filterAppointments(date1,date2)
+
+  
+  res.json(filteredAppointments);
+})
+
+ 
+app.delete("/appointments", (req, res) => {
+  const {code} = req.query; 
+  if (!code) {
+    return res.status(400).json({ message: 'Falta codigo de cita' });
+  }
+
+  const index = appointments.findIndex(item => item.code === code);
+S
+
+  if(index !== -1){
+    appointments[index].status=false;
+    return res.status(400).json({ message: "cita "+code+" cancelada con exito"});
+  } else{
+    return res.status(400).json({ message: 'cita no encontrada' });
+  }
+})
+
+
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
+
